b55c85bc2f1e9da2f489c3616ad3edaca614663c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-12 17:00:55 +0200

    custom font

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'IndieFlower'),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Adding Assets'),
@@ -18,6 +19,18 @@ class MyApp extends StatelessWidget {
                 image: AssetImage('assets/images/sunset.jpg'),
               ),
               Image.asset('assets/icons/baltika.jpg'),
+              Positioned(
+                top: 16,
+                left: 115,
+                child: Text(
+                    'My custom font',
+                     style: TextStyle(
+                       fontSize: 30,
+                       color: Colors.white,
+                       //fontFamily: 'IndieFlower',
+                     ),
+                ),
+              )
             ],
           ),
         ),
