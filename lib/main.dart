5a4aa7ebfa679d9f1221df90fdc0f1c62fa086ea
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-12 16:46:45 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Adding Assets'),
+        ),
+        body: Center(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/images/sunset.jpg'),
+              ),
+              Image.asset('assets/icons/baltika.jpg'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
